37d3fd35748e0e1e01ae79f62963fea4a5f67039
Matthew Scott <matt@11craft.com>
2013-06-13 23:14:01 -0500

    Add 'usda.facts' module and 'usda_tags' to work with nutrition facts in templates

---
+++ usda/facts.py
@@ -0,0 +1,44 @@
+from .models import Nutrient, NutrientData
+
+
+ENERGY = 208
+TOTAL_FAT = 204
+
+
+class NutritionFacts(object):
+
+    def __init__(self, food, calories=None, grams=100):
+        self.food = food
+        self._calories = calories
+        self._grams = grams
+        energy_nutrient = Nutrient.objects.get(number=208)
+        self.energy_100g = NutrientData.objects.get(food=food, nutrient=energy_nutrient).nutrient_value
+        self.data_cache = {}
+
+    @property
+    def calories(self):
+        return self._calories
+
+    @calories.setter
+    def calories(self, val):
+        self._calories = val
+        self._grams = 100.0 * val / self.energy_100g
+
+    @property
+    def grams(self):
+        return self._grams
+
+    @grams.setter
+    def grams(self, val):
+        self._calories = self.energy_100g * val / 100.0
+        self._grams = val
+
+    @property
+    def totalfat_g(self):
+        return self._nutrient_amount(TOTAL_FAT)
+
+    def _nutrient_amount(self, number):
+        if number not in self.data_cache:
+            self.data_cache[number] = NutrientData.objects.get(
+                food=self.food, nutrient__number=number).nutrient_value
+        return self.data_cache[number] * self.grams / 100.0
